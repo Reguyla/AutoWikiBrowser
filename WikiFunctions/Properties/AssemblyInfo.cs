bad39b79b134767e70b7ca58f33967b9beac45b4
Reguyla <reguyla@gmail.com>
2026-06-27 20:38:38 -0400

    Sync with [r13020] at Sourceforge. Release version 6.5.0.0

--- WikiFunctions/Properties/AssemblyInfo.cs
+++ WikiFunctions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WikiFunctions")]
-[assembly: AssemblyCopyright("Copyright © 2006-2025 AutoWikiBrowser Team")]
+[assembly: AssemblyCopyright("Copyright © 2006-2026 AutoWikiBrowser Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.4.0.1")]
-[assembly: AssemblyFileVersion("6.4.0.1")]
+[assembly: AssemblyVersion("6.5.0.0")]
+[assembly: AssemblyFileVersion("6.5.0.0")]
 
 [assembly: CLSCompliant(true)]
