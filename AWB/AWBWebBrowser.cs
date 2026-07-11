abf24ee1f7f86ff94db0f5fcff309b3454f263b9
Reguyla <reguyla@gmail.com>
2026-07-11 14:00:36 -0400

    logic : Multiple : isolate or replace browser selection logic

--- AWB/AWBWebBrowser.cs
+++ AWB/AWBWebBrowser.cs
@@ -5,37 +5,55 @@ namespace AutoWikiBrowser
 {
     class AWBWebBrowser : WebBrowser
     {
-        public override bool PreProcessMessage(ref Message msg) 
+        private const int WmKeyUp = 0x0101;
+
+        public override bool PreProcessMessage(ref Message msg)
         {
-            // look for and intercept a Ctrl+C key up event to copy selected text to keyboard
-            if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.C && ModifierKeys == Keys.Control)
+            bool isKeyUp = msg.Msg == WmKeyUp;
+            bool isControlPressed = ModifierKeys == Keys.Control;
+            int keyCode = msg.WParam.ToInt32();
+
+            if (isKeyUp && isControlPressed && keyCode == (int)Keys.C)
             {
                 CopySelectedText();
                 return true;
             }
 
-            // Ctrl+J to find selected text in edit text box
-            if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.J && ModifierKeys == Keys.Control && TextSelected())
+            if (isKeyUp &&
+                isControlPressed &&
+                keyCode == (int)Keys.J &&
+                TextSelected())
             {
-                Variables.MainForm.EditBox.Find(SelectedText(), false, false, Variables.MainForm.TheSession.Page.Title);
+                Variables.MainForm.EditBox.Find(
+                    SelectedText(),
+                    false,
+                    false,
+                    Variables.MainForm.TheSession.Page.Title);
+
                 return true;
             }
 
-            // Ctrl+S passed through
-            if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.S && ModifierKeys == Keys.Control)
+            if (isKeyUp && isControlPressed && keyCode == (int)Keys.S)
             {
-                if(Variables.MainForm.SaveButton.Enabled)
+                if (Variables.MainForm.SaveButton.Enabled)
+                {
                     Variables.MainForm.Save("AWBWebBrowser");
-                else if(Variables.MainForm.StartButton.Enabled)
+                }
+                else if (Variables.MainForm.StartButton.Enabled)
+                {
                     Variables.MainForm.Start("AWBWebBrowser");
+                }
+
                 return true;
             }
 
-            // Ctrl+I passed through
-            if (msg.Msg == 0x101 && msg.WParam.ToInt32() == (int)Keys.I && ModifierKeys == Keys.Control)
+            if (isKeyUp && isControlPressed && keyCode == (int)Keys.I)
             {
-                if(Variables.MainForm.SkipButton.Enabled)
+                if (Variables.MainForm.SkipButton.Enabled)
+                {
                     Variables.MainForm.SkipPage("AWBWebBrowser", "user");
+                }
+
                 return true;
             }
 
@@ -43,7 +61,7 @@ namespace AutoWikiBrowser
         }
 
         /// <summary>
-        /// Copies the selected text (if any) to the clipboard
+        /// Copies the selected text, if any, to the clipboard.
         /// </summary>
         private void CopySelectedText()
         {
@@ -86,49 +104,49 @@ namespace AutoWikiBrowser
             }
         }
 
-        /// <summary>
-        /// Returns whether there is currently any text selected
-        /// in the embedded browser.
-        /// </summary>
-        /// <remarks>
-        /// Browser-specific selection retrieval is delegated to
-        /// <see cref="BrowserSelectionProvider"/>.
-        /// </remarks>
-        /// <returns>
-        /// <c>true</c> if text is currently selected; otherwise, <c>false</c>.
-        /// </returns>
         private bool TextSelectedChecked()
         {
-            return !string.IsNullOrEmpty(TextRange());
+            return !string.IsNullOrEmpty(GetSelectedText());
         }
 
         private string SelectedText()
         {
-            var range = TextRange();
-
-            return string.IsNullOrEmpty(range) ? "" : range;
+            return GetSelectedText() ?? string.Empty;
         }
-                private string TextRange()
+
+        private string GetSelectedText()
         {
             return BrowserSelectionProvider.GetSelectedText(Document);
         }
 
         public override void Refresh()
         {
-            // webbrowser Refresh calls fail under Mono so silently ignore for the moment
-            if(!Globals.UsingMono)
+            // Legacy WebBrowser refresh calls may fail under Mono,
+            // so skip the operation when running under Mono.
+            if (!Globals.UsingMono)
+            {
                 base.Refresh();
+            }
         }
 
         public new void Navigate(string urlString)
         {
-            // webbrowser Navigate calls fail under Mono so silently ignore for the moment
+            // Legacy WebBrowser navigation calls may fail under Mono,
+            // so skip the operation when running under Mono.
             if (!Globals.UsingMono)
-                base.Navigate(urlString, null, null,
-                    string.Format("User-Agent: AWBWebBrowser {0}/{1} {2}\r\n",
-                        System.Reflection.Assembly.GetExecutingAssembly().GetName(), Tools.VersionString,
-                        Tools.DefaultUserAgentString)
-                );
+            {
+                base.Navigate(
+                    urlString,
+                    null,
+                    null,
+                    string.Format(
+                        "User-Agent: AWBWebBrowser {0}/{1} {2}\r\n",
+                        System.Reflection.Assembly
+                            .GetExecutingAssembly()
+                            .GetName(),
+                        Tools.VersionString,
+                        Tools.DefaultUserAgentString));
+            }
         }
     }
 }
