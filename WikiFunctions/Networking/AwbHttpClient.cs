13b1a1e933b593e2a48ac0f2a2845556ad3457c9
Reguyla <reguyla@gmail.com>
2026-07-13 09:20:31 -0400

    Logic : Wikifunctions.Networking : create shared HTTP client infrastructure

---
+++ WikiFunctions/Networking/AwbHttpClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using WikiFunctions.Plugin;
+
+namespace WikiFunctions.Networking
+{
+    internal static class AwbHttpClient
+    {
+        public static string GetString(
+            string url,
+            Encoding encoding,
+            out string responseUrl,
+            IAutoWikiBrowser awb = null)
+        {
+            // Migrated GetHTML implementation.
+        }
+
+        public static string PostForm(
+            NameValueCollection values,
+            string url)
+        {
+            // Migrated PostData implementation.
+        }
+
+        public static void RefreshProxy()
+        {
+            // Migrated proxy discovery.
+        }
+    }
+}
