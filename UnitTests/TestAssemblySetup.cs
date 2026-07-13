540fda6f318811368cbbeb4b16a32a459eca63d1
Reguyla <reguyla@gmail.com>
2026-07-13 15:46:21 -0400

    test : UnitTests : Register legacy code page provider

---
+++ UnitTests/TestAssemblySetup.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [SetUpFixture]
+    public sealed class TestAssemblySetup
+    {
+        [OneTimeSetUp]
+        public void RegisterEncodingProvider()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+    }
+}
