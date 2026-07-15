70f6d962e9488923be8721cf54a5355ff9c63cd9
Reguyla <reguyla@gmail.com>
2026-07-15 18:12:40 -0400

    docs : List providers : Improve provider interface documentation

--- WikiFunctions/Lists/Providers/IListProvider.cs
+++ WikiFunctions/Lists/Providers/IListProvider.cs
@@ -8,77 +8,93 @@ the Free Software Foundation; either version 2 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 namespace WikiFunctions.Lists.Providers;
 
 /// <summary>
-/// An interface implemented by objects which attach to the WikiFunctions' ListMaker combo box and return lists of pages
+/// Defines a provider that integrates with the ListMaker source selector
+/// and produces a list of wiki articles.
 /// </summary>
 public interface IListProvider
 {
     /// <summary>
-    /// Process the user input (if any) and return a list of pages
+    /// Processes the supplied search criteria and creates a list of articles.
     /// </summary>
-    /// <param name="searchCriteria">User Input/Pages</param>
+    /// <param name="searchCriteria">
+    /// The user-entered values or pages used by the provider.
+    /// </param>
+    /// <returns>The articles produced by the provider.</returns>
     List<Article> MakeList(params string[] searchCriteria);
 
     /// <summary>
-    /// The text to display as the combobox list item
+    /// Gets the text displayed for this provider in the source selection
+    /// combo box.
     /// </summary>
     string DisplayText { get; }
 
     /// <summary>
-    /// The text to display inside the Select Source text box
+    /// Gets the text displayed beside the source input field.
     /// </summary>
     string UserInputTextBoxText { get; }
 
     /// <summary>
-    /// Indicates whether the Select Source text box should be enabled
+    /// Gets whether the source input field should be enabled.
     /// </summary>
     bool UserInputTextBoxEnabled { get; }
 
     /// <summary>
-    /// Called when the ListMaker Provider has been selected in the ComboBox
+    /// Performs any provider-specific action required when the provider
+    /// is selected.
     /// </summary>
     void Selected();
 
     /// <summary>
-    /// True if the object expects to be started on a separate thread
+    /// Gets whether list generation should run on a separate thread.
     /// </summary>
     bool RunOnSeparateThread { get; }
 
     /// <summary>
-    /// Whether to strip the URL of the current site from the input
+    /// Gets whether the current wiki URL should be removed from input values.
     /// </summary>
     bool StripUrl { get; }
 }
 
-/// <summary>
-/// Extension of IListProvider to provide some extra functionality needed for options on Special Pages
-/// </summary>
-interface ISpecialPageProvider : IListProvider
+    /// <summary>
+    /// Extends <see cref="IListProvider"/> with options required by
+    /// MediaWiki special-page providers.
+    /// </summary>
+    interface ISpecialPageProvider : IListProvider
 {
     /// <summary>
-    /// Process the user input (if any) and return a list of pages
+    /// Processes the supplied search criteria for a specific namespace
+    /// and creates a list of articles.
     /// </summary>
-    /// <param name="namespace">Namespace to enumerate</param>
-    /// <param name="searchCriteria">User Input/Pages</param>
-    List<Article> MakeList(int @namespace, params string[] searchCriteria);
+    /// <param name="namespace">
+    /// The namespace identifier used to filter or enumerate results.
+    /// </param>
+    /// <param name="searchCriteria">
+    /// The user-entered values or pages used by the provider.
+    /// </param>
+    /// <returns>The articles produced by the provider.</returns>
+    List<Article> MakeList(
+        int @namespace,
+        params string[] searchCriteria);
 
     /// <summary>
-    /// Whether something must be entered in the text box
+    /// Gets whether the provider requires text to be entered in the source
+    /// input field.
     /// </summary>
     bool PagesNeeded { get; }
 
     /// <summary>
-    /// Whether Namespace box should be enabled, therefore whether namespace can be used to filter results
+    /// Gets whether the namespace selector should be enabled.
     /// </summary>
     bool NamespacesEnabled { get; }
 }
