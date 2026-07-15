b820c80a68618ef740846c8c7d48237cd8f7088d
Reguyla <reguyla@gmail.com>
2026-07-15 17:41:02 -0400

    modernize : JSON list providers : Remove obsolete code and simplify API list parsing

--- WikiFunctions/Lists/Providers/ApiJsonListProviderBase.cs
+++ WikiFunctions/Lists/Providers/ApiJsonListProviderBase.cs
@@ -1,48 +1,35 @@
-﻿/*
-Copyright (C) 2015 Sam Reed
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using WikiFunctions.API;
 
 namespace WikiFunctions.Lists.Providers;
 
+/// <summary>
+/// Provides article lists from MediaWiki API responses formatted as JSON.
+/// </summary>
 public abstract class ApiJsonListProviderBase : IListProvider
 {
-    protected ApiJsonListProviderBase()
-    {
-        Limit = 1000;
-    }
+    /// <summary>
+    /// Gets or sets the maximum number of pages to return.
+    /// </summary>
+    public int Limit { get; set; } = 1000;
 
     /// <summary>
-    /// Upper limit for number of pages returned, could be a bit exceeded by number of pages in the last request
+    /// Gets the JSON property containing the page title.
     /// </summary>
-    public int Limit { get; set; }
-
-    protected string WantedAttribute = "title";
+    protected virtual string WantedAttribute => "title";
 
     /// <summary>
-    /// Main function that retrieves the list from API, including paging
+    /// Retrieves a list of pages from a MediaWiki API JSON response.
     /// </summary>
-    /// <param name="url">URL of API request</param>
-    /// <param name="haveSoFar">Number of pages already retrieved, for upper limit control</param>
-    /// <returns>List of pages</returns>
-    public List<Article> ApiMakeList(string url, int haveSoFar)
+    /// <param name="url">The API request query string.</param>
+    /// <param name="haveSoFar">
+    /// The number of pages already retrieved by the current list operation.
+    /// </param>
+    /// <returns>The pages returned by the API.</returns>
+    public List<Article> ApiMakeList(
+        string url,
+        int haveSoFar)
     {
         if (Globals.UnitTestMode)
         {
@@ -56,14 +43,14 @@ public abstract class ApiJsonListProviderBase : IListProvider
         string responseJson = editor.QueryApiJson(url);
 
         if (string.IsNullOrWhiteSpace(responseJson))
-            return new List<Article>();
+            return [];
 
         JObject json;
 
         try
         {
-            using var stringReader = new StringReader(responseJson);
-            using var jsonReader = new JsonTextReader(stringReader)
+            using StringReader stringReader = new(responseJson);
+            using JsonTextReader jsonReader = new(stringReader)
             {
                 MaxDepth = 64,
                 DateParseHandling = DateParseHandling.None
@@ -71,32 +58,32 @@ public abstract class ApiJsonListProviderBase : IListProvider
 
             json = JObject.Load(jsonReader);
         }
-        catch (JsonReaderException)
+        catch (JsonReaderException ex)
         {
-            return new List<Article>();
+            Tools.WriteDebug(
+                nameof(ApiMakeList),
+                ex.ToString());
+
+            return [];
         }
 
         if (json["query"]?["pageswithprop"] is not JArray pages)
-            return new List<Article>();
+            return [];
 
         return pages
             .OfType<JObject>()
-            .Select(page => page["title"])
-            .Where(title => title?.Type == JTokenType.String)
-            .Select(title => title!.Value<string>())
-            .Where(title => !string.IsNullOrWhiteSpace(title))
-            .Select(title => new Article(title!))
+            .Select(page => page[WantedAttribute])
+            .Where(value => value?.Type == JTokenType.String)
+            .Select(value => value!.Value<string>())
+            .Where(value => !string.IsNullOrWhiteSpace(value))
+            .Select(value => new Article(value!))
             .ToList();
     }
 
-    public virtual bool StripUrl
-    {
-        get { return false; }
-    }
+    public virtual bool StripUrl => false;
 
-    #region To be overridden
-
-    public abstract List<Article> MakeList(params string[] searchCriteria);
+    public abstract List<Article> MakeList(
+        params string[] searchCriteria);
 
     public abstract string DisplayText { get; }
 
@@ -106,67 +93,46 @@ public abstract class ApiJsonListProviderBase : IListProvider
 
     public abstract void Selected();
 
-    public virtual bool RunOnSeparateThread
-    { get { return true; } }
-
-    #endregion
+    public virtual bool RunOnSeparateThread => true;
 }
 
 /// <summary>
-/// 
+/// Retrieves pages that have one or more specified MediaWiki page properties.
 /// </summary>
-/// <remarks>
-/// Temporary, for debugging
-/// </remarks>
-public class PagesWithPropJsonListProvider : ApiJsonListProviderBase
+public sealed class PagesWithPropJsonListProvider
+    : ApiJsonListProviderBase
 {
-    #region Tags: <pageswithprop>/<page>
-    //protected override ICollection<string> PageElements
-    //{
-    //    get { return new[] { "page" }; }
-    //}
-
-    //protected override ICollection<string> Actions
-    //{
-    //    get { return new[] { "pageswithprop" }; }
-    //}
-    #endregion
-
-    public override List<Article> MakeList(params string[] searchCriteria)
+    public override List<Article> MakeList(
+        params string[] searchCriteria)
     {
-        List<Article> list = new List<Article>();
+        List<Article> list = new();
 
         foreach (string prop in searchCriteria)
         {
-            string url = "list=pageswithprop&pwppropname="
-                         + WebUtility.UrlEncode(prop) + "&pwplimit=max";
+            string url =
+                $"list=pageswithprop&pwppropname={WebUtility.UrlEncode(prop)}&pwplimit=max";
 
-            list.AddRange(ApiMakeList(url, list.Count));
+            list.AddRange(
+                ApiMakeList(url, list.Count));
         }
+
         return list;
     }
 
-    public List<Article> MakeList(int @namespace, params string[] searchCriteria)
-    {
-        return MakeList(searchCriteria);
-    }
+    public List<Article> MakeList(
+        int @namespace,
+        params string[] searchCriteria) =>
+        MakeList(searchCriteria);
 
-    #region ListMaker properties
-    public override string DisplayText
-    {
-        get { return "(JSON)Pages with a page property"; }
-    }
+    public override string DisplayText =>
+        "(JSON)Pages with a page property";
 
-    public override string UserInputTextBoxText
-    {
-        get { return "Property name:"; }
-    }
+    public override string UserInputTextBoxText =>
+        "Property name:";
 
-    public override bool UserInputTextBoxEnabled
-    {
-        get { return true; }
-    }
+    public override bool UserInputTextBoxEnabled => true;
 
-    public override void Selected() { }
-    #endregion
+    public override void Selected()
+    {
+    }
 }
