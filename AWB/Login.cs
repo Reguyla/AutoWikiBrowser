ae5cd0f5807732464373dbbd452215f6ad91ed22
Reguyla <reguyla@gmail.com>
2026-08-01 14:30:14 -0400

    cleanup : Login : Improve docs and minor improvements

--- AWB/Login.cs
+++ AWB/Login.cs
@@ -3,13 +3,28 @@ using WikiFunctions;
 
 namespace AutoWikiBrowser;
 
+/// <summary>
+/// Presents a dialog for entering HTTP authentication credentials.
+/// </summary>
 internal sealed partial class Login : Form
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Login"/> class.
+    /// </summary>
     public Login()
     {
         InitializeComponent();
     }
 
+    /// <summary>
+    /// Closes the dialog when the Enter key is pressed.
+    /// </summary>
+    /// <param name="sender">
+    /// The control that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// Event data containing the released key.
+    /// </param>
     private void FormOnKeyUp(object sender, KeyEventArgs e)
     {
         if (e.KeyCode == Keys.Enter)
@@ -18,15 +33,30 @@ internal sealed partial class Login : Form
         }
     }
 
+    /// <summary>
+    /// Saves the entered credentials and closes the dialog when the
+    /// <c>Login</c> button is clicked.
+    /// </summary>
+    /// <param name="sender">
+    /// The control that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// Event data for the button click.
+    /// </param>
     private void btnLogin_Click(object sender, EventArgs e)
     {
         CloseForm();
     }
 
+    /// <summary>
+    /// Stores the entered HTTP authentication credentials and closes the
+    /// dialog.
+    /// </summary>
     private void CloseForm()
     {
         Variables.HttpAuthUsername = txtUsername.Text;
         Variables.HttpAuthPassword = txtPassword.Text;
+
         Close();
     }
 }
