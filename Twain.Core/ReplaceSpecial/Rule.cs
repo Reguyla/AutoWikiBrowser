198a8eed9b97c9ac93d015b01c51b8fd92b41952
Reguyla <reguyla@gmail.com>
2026-08-07 10:37:58 -0400

    cleanup : Rule : Improve docs; Modernized object creation; Standardized empty string initialization; Simplified the application count limit and several other small improvements

--- Twain.Core/ReplaceSpecial/Rule.cs
+++ Twain.Core/ReplaceSpecial/Rule.cs
@@ -1,20 +1,20 @@
 ﻿/*
-    Derived from Autowikibrowser
-    Copyright (C) 2007 Martin Richards
+Derived from Autowikibrowser
+Copyright (C) 2007 Martin Richards
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 using System.Windows.Forms;
@@ -22,126 +22,267 @@ using Twain.Core.Parse;
 
 namespace Twain.Core.ReplaceSpecial;
 
+/// <summary>
+/// Defines a replacement rule that can operate on the entire page or within
+/// individual template invocations.
+/// </summary>
 public class Rule : IRule
 {
-    public enum T { OnWholePage = 0, InsideTemplate };
+    /// <summary>
+    /// Specifies where the rule should be applied.
+    /// </summary>
+    public enum T
+    {
+        /// <summary>
+        /// Applies the rule to the entire page.
+        /// </summary>
+        OnWholePage = 0,
+
+        /// <summary>
+        /// Applies the rule separately within template invocations.
+        /// </summary>
+        InsideTemplate
+    }
 
     public T ruletype_ = T.OnWholePage;
 
-    public string replace_ = "", with_ = "", ifContains_ = "", ifNotContains_ = string.Empty;
+    public string replace_ = string.Empty;
+    public string with_ = string.Empty;
+    public string ifContains_ = string.Empty;
+    public string ifNotContains_ = string.Empty;
 
-    public bool regex_, ifIsRegex_;
+    public bool regex_;
+    public bool ifIsRegex_;
 
     public int numoftimes_ = 1;
 
-    public RegexOptions ifRegexOptions_ = RegexOptions.None, regexOptions_ = RegexOptions.None;
+    public RegexOptions ifRegexOptions_ = RegexOptions.None;
+    public RegexOptions regexOptions_ = RegexOptions.None;
 
-    RuleControl ruleControl_;
+    private RuleControl? ruleControl_;
 
-    public override Object Clone()
-    {
-        Rule res = (Rule)MemberwiseClone();
-        res.ruleControl_ = null;
-        return res;
-    }
-
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Rule"/> class.
+    /// </summary>
     public Rule()
     {
         Name = "Rule";
     }
 
+    /// <summary>
+    /// Creates a shallow copy of this rule without retaining its associated
+    /// user interface control.
+    /// </summary>
+    /// <returns>
+    /// A shallow copy of the current rule.
+    /// </returns>
+    public override object Clone()
+    {
+        Rule clone = (Rule)MemberwiseClone();
+        clone.ruleControl_ = null;
+
+        return clone;
+    }
+
+    /// <summary>
+    /// Gets the user interface control currently associated with this rule.
+    /// </summary>
+    /// <returns>
+    /// The associated rule control, or <see langword="null"/> if no control is
+    /// currently associated with the rule.
+    /// </returns>
     public override Control GetControl()
     {
         return ruleControl_;
     }
 
+    /// <summary>
+    /// Removes the association between this rule and its current user
+    /// interface control.
+    /// </summary>
     public override void ForgetControl()
     {
         ruleControl_ = null;
     }
 
-    public override Control CreateControl(IRuleControlOwner owner, Control.ControlCollection collection, System.Drawing.Point pos)
+    /// <summary>
+    /// Creates and initializes the user interface control for this rule.
+    /// </summary>
+    /// <param name="owner">
+    /// The owner responsible for hosting the rule control.
+    /// </param>
+    /// <param name="collection">
+    /// The control collection that will contain the created control.
+    /// </param>
+    /// <param name="pos">
+    /// The initial location of the control.
+    /// </param>
+    /// <returns>
+    /// The newly created rule control.
+    /// </returns>
+    public override Control CreateControl(
+        IRuleControlOwner owner,
+        Control.ControlCollection collection,
+        System.Drawing.Point pos)
     {
-        RuleControl rc = new RuleControl(owner) { Location = pos };
-        rc.RestoreFromRule(this);
+        RuleControl control = new(owner)
+        {
+            Location = pos
+        };
+
+        control.RestoreFromRule(this);
+
         DisposeControl();
-        ruleControl_ = rc;
-        collection.Add(rc);
-        return rc;
+
+        ruleControl_ = control;
+        collection.Add(control);
+
+        return control;
     }
 
+    /// <summary>
+    /// Saves the current rule-control values to this rule.
+    /// </summary>
     public override void Save()
     {
-        if (ruleControl_ == null)
+        if (ruleControl_ is null)
+        {
             return;
+        }
+
         ruleControl_.SaveToRule(this);
     }
 
+    /// <summary>
+    /// Restores this rule's values to the associated rule control.
+    /// </summary>
     public override void Restore()
     {
-        if (ruleControl_ == null)
+        if (ruleControl_ is null)
+        {
             return;
+        }
+
         ruleControl_.RestoreFromRule(this);
     }
 
+    /// <summary>
+    /// Selects the rule name in the associated rule control.
+    /// </summary>
     public override void SelectName()
     {
-        if (ruleControl_ == null)
+        if (ruleControl_ is null)
+        {
             return;
+        }
+
         ruleControl_.SelectName();
     }
 
-    public override string Apply(TreeNode tn, string text, string title)
+    /// <summary>
+    /// Applies this rule to the supplied text the configured number of times.
+    /// </summary>
+    /// <param name="tn">
+    /// The tree node associated with the rule being applied.
+    /// </param>
+    /// <param name="text">
+    /// The text to process.
+    /// </param>
+    /// <param name="title">
+    /// The title of the page being processed.
+    /// </param>
+    /// <returns>
+    /// The processed text, or the original text when the rule is disabled,
+    /// the input is empty, or the configured application count is not
+    /// positive.
+    /// </returns>
+    public override string Apply(
+        TreeNode tn,
+        string text,
+        string title)
     {
         if (string.IsNullOrEmpty(text) || !enabled_)
+        {
             return text;
+        }
 
-        int apply = numoftimes_;
+        int applyCount = Math.Min(numoftimes_, 100);
 
-        if (apply > 100)
-            apply = 100;
-        if (apply <= 0)
+        if (applyCount <= 0)
+        {
             return text;
+        }
 
-        for (int j = 0; j != apply; ++j)
+        for (int index = 0; index < applyCount; index++)
         {
             text = ApplyOnce(tn, text, title);
         }
+
         return text;
     }
 
     /// <summary>
-    ///
+    /// Applies this rule once using the configured rule type.
     /// </summary>
-    /// <param name="tn"></param>
-    /// <param name="text"></param>
-    /// <param name="title"></param>
-    /// <returns></returns>
-    static string ApplyOnce(TreeNode tn, string text, string title)
+    /// <param name="tn">
+    /// The tree node associated with the rule being applied.
+    /// </param>
+    /// <param name="text">
+    /// The text to process.
+    /// </param>
+    /// <param name="title">
+    /// The title of the page being processed.
+    /// </param>
+    /// <returns>
+    /// The processed text.
+    /// </returns>
+    private static string ApplyOnce(
+        TreeNode tn,
+        string text,
+        string title)
     {
-        Rule r = (Rule)tn.Tag;
+        Rule rule = (Rule)tn.Tag;
 
-        if (r.ruletype_ == T.OnWholePage)
+        if (rule.ruletype_ == T.OnWholePage)
+        {
             return ApplyOn(tn, text, title);
+        }
 
-        return (r.ruletype_ == T.InsideTemplate) ? ApplyInsideTemplate(tn, text, title) : text;
+        return rule.ruletype_ == T.InsideTemplate
+            ? ApplyInsideTemplate(tn, text, title)
+            : text;
     }
 
     /// <summary>
-    /// Applies logic of rule inside templates: to all template calls including any level of nesting
+    /// Applies the rule independently to each template invocation found in the
+    /// supplied text, including nested template calls.
     /// </summary>
-    /// <param name="tn"></param>
-    /// <param name="text"></param>
-    /// <param name="title"></param>
-    /// <returns></returns>
-    private static string ApplyInsideTemplate(TreeNode tn, string text, string title)
+    /// <param name="tn">
+    /// The tree node associated with the rule being applied.
+    /// </param>
+    /// <param name="text">
+    /// The text containing the template invocations to process.
+    /// </param>
+    /// <param name="title">
+    /// The title of the page being processed.
+    /// </param>
+    /// <returns>
+    /// The text after applicable template invocations have been processed.
+    /// </returns>
+    private static string ApplyInsideTemplate(
+        TreeNode tn,
+        string text,
+        string title)
     {
         string result = text;
-        foreach (string m in Parsers.GetAllTemplateDetail(text))
+
+        foreach (string template in Parsers.GetAllTemplateDetail(text))
         {
-            if (CheckIf(tn, m))
+            if (CheckIf(tn, template))
             {
-                result = result.Replace(m, ApplyOn(tn, m, title));
+                result = result.Replace(
+                    template,
+                    ApplyOn(tn, template, title));
             }
         }
 
@@ -149,79 +290,155 @@ public class Rule : IRule
     }
 
     /// <summary>
-    ///
+    /// Determines whether the supplied text satisfies the rule's optional
+    /// contains and does-not-contain conditions.
     /// </summary>
-    /// <param name="tn"></param>
-    /// <param name="text"></param>
-    /// <returns></returns>
+    /// <param name="tn">
+    /// The tree node associated with the rule being evaluated.
+    /// </param>
+    /// <param name="text">
+    /// The text to evaluate.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if all configured conditions are satisfied;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
     private static bool CheckIf(TreeNode tn, string text)
     {
-        Rule r = (Rule)tn.Tag;
+        Rule rule = (Rule)tn.Tag;
 
-        StringComparison sc = (((int)r.ifRegexOptions_ & (int)RegexOptions.IgnoreCase) != 0)
-                                  ? StringComparison.OrdinalIgnoreCase
-                                  : StringComparison.Ordinal;
+        StringComparison comparison =
+            (rule.ifRegexOptions_ & RegexOptions.IgnoreCase) != 0
+                ? StringComparison.OrdinalIgnoreCase
+                : StringComparison.Ordinal;
 
-        if (!string.IsNullOrEmpty(r.ifContains_))
+        if (!string.IsNullOrEmpty(rule.ifContains_))
         {
-            if ((r.ifIsRegex_ && !Regex.IsMatch(text, r.ifContains_, r.ifRegexOptions_))
-                || (!r.ifIsRegex_ && text.IndexOf(r.ifContains_, sc) < 0))
+            bool contains =
+                rule.ifIsRegex_
+                    ? Regex.IsMatch(
+                        text,
+                        rule.ifContains_,
+                        rule.ifRegexOptions_)
+                    : text.IndexOf(
+                        rule.ifContains_,
+                        comparison) >= 0;
+
+            if (!contains)
+            {
                 return false;
+            }
         }
-        if (!string.IsNullOrEmpty(r.ifNotContains_))
+
+        if (!string.IsNullOrEmpty(rule.ifNotContains_))
         {
-            if ((r.ifIsRegex_ && Regex.IsMatch(text, r.ifNotContains_, r.ifRegexOptions_))
-                || (!r.ifIsRegex_ && text.IndexOf(r.ifNotContains_, sc) >= 0))
+            bool containsExcludedText =
+                rule.ifIsRegex_
+                    ? Regex.IsMatch(
+                        text,
+                        rule.ifNotContains_,
+                        rule.ifRegexOptions_)
+                    : text.IndexOf(
+                        rule.ifNotContains_,
+                        comparison) >= 0;
+
+            if (containsExcludedText)
+            {
                 return false;
+            }
         }
 
         return true;
     }
 
     /// <summary>
-    ///
+    /// Applies the configured replacement and then applies any child rules to
+    /// the resulting text.
     /// </summary>
-    /// <param name="tn"></param>
-    /// <param name="text"></param>
-    /// <param name="title"></param>
-    /// <returns></returns>
-    private static string ReplaceOn(TreeNode tn, string text, string title)
+    /// <param name="tn">
+    /// The tree node associated with the rule being applied.
+    /// </param>
+    /// <param name="text">
+    /// The text to process.
+    /// </param>
+    /// <param name="title">
+    /// The title of the page being processed.
+    /// </param>
+    /// <returns>
+    /// The processed text after the configured replacement and child rules
+    /// have been applied.
+    /// </returns>
+    private static string ReplaceOn(
+        TreeNode tn,
+        string text,
+        string title)
     {
-        Rule r = (Rule)tn.Tag;
+        Rule rule = (Rule)tn.Tag;
 
-        if (!string.IsNullOrEmpty(r.replace_))
+        if (!string.IsNullOrEmpty(rule.replace_))
         {
-            string replace = Tools.ApplyKeyWords(title, r.replace_, true);
+            string pattern =
+                Tools.ApplyKeyWords(
+                    title,
+                    rule.replace_,
+                    true);
 
-            string with = Tools.ApplyKeyWords(title, r.with_);
+            string replacement =
+                Tools.ApplyKeyWords(
+                    title,
+                    rule.with_);
 
-            if (!r.regex_)
-                replace = Regex.Escape(replace);
+            if (!rule.regex_)
+            {
+                pattern = Regex.Escape(pattern);
+            }
 
-            // stop \r\n being interpreted literally
-            with = with.Replace(@"\r", "\r").Replace(@"\n", "\n");
+            // Convert escaped newline sequences into actual newline
+            // characters before applying the replacement.
+            replacement = replacement
+                .Replace(@"\r", "\r")
+                .Replace(@"\n", "\n");
 
-            text = Regex.Replace(text, replace, with, r.regexOptions_);
+            text = Regex.Replace(
+                text,
+                pattern,
+                replacement,
+                rule.regexOptions_);
         }
 
-        foreach (TreeNode t in tn.Nodes)
+        foreach (TreeNode childNode in tn.Nodes)
         {
-            IRule sr = (IRule)t.Tag;
-            text = sr.Apply(t, text, title);
+            IRule childRule = (IRule)childNode.Tag;
+            text = childRule.Apply(childNode, text, title);
         }
 
         return text;
     }
 
     /// <summary>
-    ///
+    /// Applies this rule when the supplied text satisfies its configured
+    /// conditions.
     /// </summary>
-    /// <param name="tn"></param>
-    /// <param name="text"></param>
-    /// <param name="title"></param>
-    /// <returns></returns>
-    private static string ApplyOn(TreeNode tn, string text, string title)
+    /// <param name="tn">
+    /// The tree node associated with the rule being applied.
+    /// </param>
+    /// <param name="text">
+    /// The text to process.
+    /// </param>
+    /// <param name="title">
+    /// The title of the page being processed.
+    /// </param>
+    /// <returns>
+    /// The processed text when the conditions are satisfied; otherwise, the
+    /// original text.
+    /// </returns>
+    private static string ApplyOn(
+        TreeNode tn,
+        string text,
+        string title)
     {
-        return !CheckIf(tn, text) ? text : ReplaceOn(tn, text, title);
+        return CheckIf(tn, text)
+            ? ReplaceOn(tn, text, title)
+            : text;
     }
 }
