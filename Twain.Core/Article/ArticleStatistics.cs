c01d1a1a6284a4d39987e4271b3a069d7b26adf2
Reguyla <reguyla@gmail.com>
2026-08-25 20:22:58 -0400

    refactor: Add article statistics model

---
+++ Twain.Core/Article/ArticleStatistics.cs
@@ -0,0 +1,32 @@
+﻿namespace Twain.Core;
+
+/// <summary>
+/// Contains basic statistics calculated from article text.
+/// </summary>
+public sealed class ArticleStatistics
+{
+    /// <summary>
+    /// Gets the number of words in the article.
+    /// </summary>
+    public int WordCount { get; init; }
+
+    /// <summary>
+    /// Gets the number of categories in the article.
+    /// </summary>
+    public int CategoryCount { get; init; }
+
+    /// <summary>
+    /// Gets the number of images in the article.
+    /// </summary>
+    public int ImageCount { get; init; }
+
+    /// <summary>
+    /// Gets the number of wikilinks in the article.
+    /// </summary>
+    public int LinkCount { get; init; }
+
+    /// <summary>
+    /// Gets the number of interwiki links in the article.
+    /// </summary>
+    public int InterwikiLinkCount { get; init; }
+}
