9ccaa5e6ff6e3f0cfa3b57e0e5a01cbda65f0246
Reguyla <reguyla@gmail.com>
2026-09-07 09:47:21 -0400

    Add ListSplitter window shell

---
+++ Twain.UI/Controls/Lists/ListSplitterWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Twain.UI.Controls.Lists
+{
+    public partial class ListSplitterWindow : Window
+    {
+        public ListSplitterWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
