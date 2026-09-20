da3ec6dffd1c47ecc4ae6f0a82534c7cbc068f69
Reguyla <reguyla@gmail.com>
2026-09-20 17:43:24 -0400

    Wire migrated tools into application shell

--- Twain.UI/Shell/ShellWindow.axaml.cs
+++ Twain.UI/Shell/ShellWindow.axaml.cs
@@ -1,5 +1,10 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Twain.CustomModules;
+using Twain.UI.Controls;
+using Twain.UI.Controls.Lists;
+using Twain.UI.DBScanner;
+using Twain.UI.ExternalPrograms;
 using Twain.UI.Profiles;
 using Twain.UI.Shell;
 
@@ -32,9 +37,63 @@ public partial class ShellWindow : Window
     }
 
     private void ExitMenuItem_Click(
-    object? sender,
-    RoutedEventArgs e)
-    {
-        Close();
-    }
+        object? sender,
+        RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    private async void MakeModuleMenuItem_Click(
+        object? sender,
+        RoutedEventArgs e)
+        {
+            CustomModule window = new();
+
+            await window.ShowDialog(this);
+        }
+
+    private async void ExternalProcessingMenuItem_Click(
+        object? sender,
+        RoutedEventArgs e)
+        {
+            ExternalProgramWindow window = new();
+
+            await window.ShowDialog<bool>(this);
+        }
+
+    private async void RegexTesterMenuItem_Click(
+        object? sender,
+        RoutedEventArgs e)
+        {
+            RegexTesterWindow window = new();
+
+            await window.ShowDialog(this);
+        }
+
+    private async void DatabaseScannerMenuItem_Click(
+        object? sender,
+        RoutedEventArgs e)
+        {
+            DatabaseScannerWindow window = new();
+
+            await window.ShowDialog(this);
+        }
+
+    private async void ListComparerMenuItem_Click(
+        object? sender,
+        RoutedEventArgs e)
+        {
+            ListComparerWindow window = new();
+
+            await window.ShowDialog(this);
+        }
+
+    private async void ListSplitterMenuItem_Click(
+        object? sender,
+        RoutedEventArgs e)
+        {
+            ListSplitterWindow window = new();
+
+            await window.ShowDialog(this);
+        }
 }
